f2c7f1899161ae55de5dd6a6da391a6e490ea138
sherzod <sher@gmail.com>
2021-12-07 17:10:11 +0500

    Salom

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>File Upload</title>
+</head>
+<body>
+ 
+<form method="post" enctype="multipart/form-data">
+    <label>Title</label>
+    <input type="text" name="title">
+    <label>File Upload</label>
+    <input type="File" name="file">
+    <input type="submit" name="submit">
+ 
+ 
+</form>
+ 
+</body>
+</html>
+ 
+<?php 
+$localhost = "localhost"; #localhost
+$dbusername = "root"; #username of phpmyadmin
+$dbpassword = "";  #password of phpmyadmin
+$dbname = "CodeFlix";  #database name
+ 
+#connection string
+$conn = mysqli_connect($localhost,$dbusername,$dbpassword,$dbname);
+ 
+if (isset($_POST["submit"]))
+ {
+     #retrieve file title
+        $title = $_POST["title"];
+     
+    #file name with a random number so that similar dont get replaced
+     $pname = rand(1000,10000)."-".$_FILES["file"]["name"];
+ 
+    #temporary file name to store file
+    $tname = $_FILES["file"]["tmp_name"];
+   
+     #upload directory path
+$uploads_dir = 'image';
+    #TO move the uploaded file to specific location
+    move_uploaded_file($tname, $uploads_dir.'/'.$pname);
+ 
+    #sql query to insert into database
+    $sql = "INSERT into fileup(images,image) VALUES('$title','$pname')";
+ 
+    if(mysqli_query($conn,$sql)){
+ 
+    echo "File Sucessfully uploaded";
+    }
+    else{
+        echo "Error";
+    }
+}
+ 
